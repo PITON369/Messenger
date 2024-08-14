82c7fb87b94d0c346ff83fe9df791c8441642ee3
Aleksandr <PITON369@yandex.ru>
2024-08-14 09:21:28 +0300

    Created class Message.

---
+++ Messenger/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Messenger
+{
+    public class Message
+    {
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+        }
+
+        public string UserName { get; set; }
+        public string MessageText{ get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public override string? ToString()
+        {
+            string output = String.Format($"{UserName} <{TimeStamp}>: {MessageText}");
+            return output;
+        }
+    }
+}
