462f89d5da4ed66c1e9e783605134d7e3f8f87b8
Alex <piton369@yandex.ru>
2024-11-19 11:41:32 +0300

    Added functionality to the WinForm client. Edit the gitignore.

--- WindowsFormsClient/Form1.cs
+++ WindowsFormsClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleMessenger;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Messenger;
 
 namespace WindowsFormsClient
 {
     public partial class Form1 : Form
     {
+        private static int MessageID = 0;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,28 @@ namespace WindowsFormsClient
 
         private void Send_Click(object sender, EventArgs e)
         {
-            UserNameTB.Text = MessageTB.Text;
+            string UserName = UserNameTB.Text;
+            string Message = MessageTB.Text;
+            if ((UserName.Length > 1) && (Message.Length > 1))
+            {
+                Messenger.Message msg = new Messenger.Message(UserName, Message, DateTime.Now);
+                API.SendMessage(msg);
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            var getMessage = new Func<Task>(async () =>
+            {
+                Messenger.Message msg = await API.GetMessageHTTPAsync(MessageID);
+                while (msg != null)
+                {
+                    MessagesLB.Items.Add(msg);
+                    MessageID++;
+                    msg = await API.GetMessageHTTPAsync(MessageID);
+                }
+            });
+            getMessage.Invoke();
         }
     }
 }
---
+++ WindowsFormsClient/MessangerClientAPI.cs
@@ -0,0 +1,119 @@
+﻿using Messenger;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMessenger
+{
+    class MessangerClientAPI
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public void TestNewtonsoftJson()
+        {
+            // Test JSon SerializeObject NewtonSoft
+            Message msg = new Message("Alex", "Hi", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+        }
+
+        public void TestNewtonsoftJsonWithWritingInFile()
+        {
+            // Test JSon SerializeObject NewtonSoft
+            Message msg = new Message("Alex", "Hi", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            // Create the file.
+            string path = @"d:\temp\ser.txt";
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(output);
+            }
+        }
+
+        public Message GetMessage(int messageId)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create($"http://localhost:5000/api/Messanger/{messageId}");
+                request.Method = "GET";
+
+                using (WebResponse response = request.GetResponse())
+                {
+                    string status = ((HttpWebResponse)response).StatusDescription;
+
+                    using (Stream dataStream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        string responseFromServer = reader.ReadToEnd();
+
+                        if (status.ToLower() == "ok" && responseFromServer != "Not found")
+                        {
+                            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseFromServer);
+                            return deserializedMsg;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return null;
+        }
+
+        public async Task<Message> GetMessageHTTPAsync(int MessageId)
+        {
+
+            var responseString = await client.GetStringAsync("http://localhost:5000/api/Messanger/" + MessageId.ToString());
+            if (responseString != null)
+            {
+                Message deserializedMsg = JsonConvert.DeserializeObject<Message>(responseString);
+                return deserializedMsg;
+            }
+            return null;
+        }
+
+        public bool SendMessage(Message msg)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create("http://localhost:5000/api/Messanger");
+                request.Method = "POST";
+
+                //msg = new Message("Alex", "Hi", DateTime.Now);
+
+                string postData = JsonConvert.SerializeObject(msg);
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+                request.ContentType = "application/json";
+                request.ContentLength = byteArray.Length;
+
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+                using (WebResponse response = request.GetResponse())
+                using (Stream dataStream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    string responseFromServer = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
