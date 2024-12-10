6fa6b9292c1ec8283f4a26be9a11b97540d96896
Alex <piton369@yandex.ru>
2024-12-10 11:55:18 +0300

    Added UWPClient.

---
+++ WPFMesenger/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Messenger
+{
+    [Serializable]
+    public class Message
+    {
+        public Message()
+        {
+            UserName = "System";
+            MessageText = "Server is running...";
+            TimeStamp = DateTime.Now;
+        }
+
+        public Message(string userName, string messageText, DateTime timeStamp)
+        {
+            UserName = userName;
+            MessageText = messageText;
+            TimeStamp = timeStamp;
+        }
+
+        public string UserName { get; set; }
+        public string MessageText{ get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public override string ToString()
+        {
+            string output = String.Format($"{UserName} <{TimeStamp}>: {MessageText}");
+            return output;
+        }
+    }
+}
